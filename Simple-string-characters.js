d76defbb10dac08882b7c4df61019f12a9898801
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-11 15:07:03 -0700

    Update Simple-string-characters.js

--- Simple-string-characters.js
+++ Simple-string-characters.js
@@ -10,3 +10,10 @@ Solve("*'&ABCDabcde12345") = [4,5,5,3].
 
 // Solution
 
+const solve = x => {
+  let u = x.match(/[A-Z]/g)||[];
+  let d = x.match(/[a-z]/g)||[];
+  let n = x.match(/[0-9]/g)||[];
+  let s = x.match(/[^A-Z0-9]/gi)||[];
+  return [u.length, d.length, n.length, s.length]
+}
