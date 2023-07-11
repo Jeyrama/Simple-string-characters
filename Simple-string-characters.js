8599798261959aa31dc99613e0e95876b7ab6f6b
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-11 15:43:57 -0700

    Update Simple-string-characters.js

--- Simple-string-characters.js
+++ Simple-string-characters.js
@@ -34,3 +34,21 @@ const solveArray = (string) => (
     return acc;
   }, {  upper: 0, lower: 0, number: 0, other: 0 }))
 )
+
+// or
+
+const solveArr = (s) => {
+  let result = [0, 0, 0, 0];
+  for (let i = 0; i < s.length; i += 1) {
+    if (s[i].toLowerCase() > s[i]) {
+      result[0] += 1;
+    } else if (s[i].toUpperCase() < s[i]) {
+      result[1] += 1;
+    } else if (s[i] >= 0 && s[i] <= 9) {
+      result[2] += 1;
+    } else {
+      result[3] += 1;
+    }
+  }
+  return result;
+};
